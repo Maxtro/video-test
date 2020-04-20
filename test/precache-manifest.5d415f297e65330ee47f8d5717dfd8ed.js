fd16a6af6c7d3578513f890992eec819890a7eda
Maxtro <adsite41@gmail.com>
2020-04-21 00:28:45 +0300

    add Git Page

---
+++ test/precache-manifest.5d415f297e65330ee47f8d5717dfd8ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfba3bfee8ebb1776602f8b6cf294592",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "b120c2caafd0f6c0a5fd",
+    "url": "/test/static/css/main.b33be5cc.chunk.css"
+  },
+  {
+    "revision": "3112165c461a9300727f",
+    "url": "/test/static/js/2.9823a3ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test/static/js/2.9823a3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b120c2caafd0f6c0a5fd",
+    "url": "/test/static/js/main.c25b414f.chunk.js"
+  },
+  {
+    "revision": "8789793586bb70881b79",
+    "url": "/test/static/js/runtime-main.c01840e7.js"
+  }
+]);
